83c4f80e6a4a047173afe9daf6a79bb95eb106bf
Michael Leo <mleo2857@gmail.com>
2018-10-02 20:01:29 -0400

    More string variables.

--- variables.js
+++ variables.js
@@ -11,3 +11,45 @@ console.log(declaredVar);
 declaredVar = 'Now I am declared';
 
 console.log(declaredVar);
+
+const myConstant = 'I cannot change';
+
+console.log(myConstant);
+
+// myConstant = 'I want to change';
+
+/* I am a
+
+multi-line comment */
+
+let myLet = 'I can be changed';
+
+console.log(myLet);
+
+myLet = 'I just changed';
+
+console.log(myLet);
+
+const myStr = 'I am a string';
+
+console.log(myStr);
+
+const doubleQuoteStr = "I have double quotes";
+
+console.log(doubleQuoteStr);
+
+const myLife = "It's my life";
+
+console.log(myLife);
+
+const alsoMyLife = 'it\'s my life';
+
+console.log(alsoMyLife);
+
+const templateStr = `" ' ' ${myLife}`;
+
+console.log(templateStr);
+
+const concatenatedStr = 'I am ' + 'concatenated';
+
+console.log(concatenatedStr);
